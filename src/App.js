a5a1ef6cc7c56ddd5883416d4d82f3034988e636
Peter Bartram Hansen <pebaha@gmail.com>
2019-10-14 16:05:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 15,
+    };
+  };
+
+  componentDidMount() {
+    this.secondsInput.focus();
+  };
+
+  handleChange(event) {
+    const seconds = (event.target.validity.valid) ? event.target.value : this.state.seconds;
+    this.setState({ seconds });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Board Game Aid</p>
+        </header>
+        <div className="App-body">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <button color="red">Secret Hitler</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button>Edit Players</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>Role Timer Seconds</p>
+          <input
+            label="test"
+            ref={(input) => { this.secondsInput = input; }}
+            type="text"
+            pattern="[0-9]*"
+            value={this.state.seconds}
+            onInput={this.handleChange.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
