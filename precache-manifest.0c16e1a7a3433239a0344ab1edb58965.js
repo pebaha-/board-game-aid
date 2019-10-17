6349df1a105443fbd2fd33a9ee605fc6a6c8bad1
Peter Bartram Hansen <pebaha@gmail.com>
2019-10-17 18:53:07 +0200

    Updates

---
+++ precache-manifest.0c16e1a7a3433239a0344ab1edb58965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95d814591df9b303b924e22222adebc",
+    "url": "/board-game-aid/index.html"
+  },
+  {
+    "revision": "3342116c41fb9fe70bb7",
+    "url": "/board-game-aid/static/css/main.26cbd387.chunk.css"
+  },
+  {
+    "revision": "3648ff7c022d12d6c8cc",
+    "url": "/board-game-aid/static/js/2.cf90e4e7.chunk.js"
+  },
+  {
+    "revision": "3342116c41fb9fe70bb7",
+    "url": "/board-game-aid/static/js/main.71f871fb.chunk.js"
+  },
+  {
+    "revision": "4a1a7a60277a72c2bfdc",
+    "url": "/board-game-aid/static/js/runtime-main.aa6a46e6.js"
+  }
+]);
