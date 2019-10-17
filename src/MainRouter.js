764f2e457e7dd87020bc6f41e54b5d2b78855bda
Peter Bartram Hansen <pebaha@gmail.com>
2019-10-17 20:04:30 +0200

    Use HashRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App.js';
 import Players from './Players.js';
-import { Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import history from './History';
 import SecretHitler from './SecretHitler.js';
 import ProtectedRoute from './ProtectedRoute.js';
@@ -9,13 +9,13 @@ import ProtectedRoute from './ProtectedRoute.js';
 class MainRouter extends React.Component {
   render() {
     return (
-      <Router history={history}>
+      <HashRouter history={history}>
         <Switch>
           <Route exact path="/" component={App} key={1} />
           <Route path="/players" component={Players} key={2} />
           <ProtectedRoute path="/sh" component={SecretHitler} key={3} />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 };
